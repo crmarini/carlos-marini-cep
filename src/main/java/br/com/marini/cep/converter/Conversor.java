c71c4f72cbdaae326f11af8d32c2cc178086f1a7
Carlos marini <raphael.marini@icloud.com>
2015-12-07 17:44:30 -0200

    Conversor

--- src/main/java/br/com/marini/cep/converter/Conversor.java
+++ src/main/java/br/com/marini/cep/converter/Conversor.java
@@ -1,8 +1,11 @@
 package br.com.marini.cep.converter;
 
+import javax.ejb.Stateless;
+
 import br.com.marini.cep.dto.EnderecoCepDTO;
 import br.com.marini.cep.model.EnderecoCep;
 
+@Stateless(name = Conversor.MAPPED_NAME, mappedName = Conversor.MAPPED_NAME)
 public interface Conversor{
 	String MAPPED_NAME = "Conversor";
 	
